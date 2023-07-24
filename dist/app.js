1edbe0af714271b5eebad46e92bbeee689f2a94b
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-07-24 23:22:02 +0100

    create an autobind decorator

--- dist/app.js
+++ dist/app.js
@@ -1,22 +1,49 @@
 "use strict";
-// function decoratorFactory () {
-//     return function decorator(target: any, descriptor: any) {
-//         // const firstTemplate = new Input('project-input');
-//         console.log('target: ', target);
-//         console.log('descriptor: ', descriptor)
-//     }
-// }
-// @decoratorFactory()
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// Decorator for autobind:
+function autobindDecorator(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjustedDescriptor;
+}
+// Input Class:
 class Input {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
         this.render();
+        this.titleInput = this.element.querySelector('#title');
+        this.descriptionInput = this.element.querySelector('#description');
+        this.peopleInput = this.element.querySelector('#people');
+        this.listen();
     }
     render() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
+    listen() {
+        this.element.addEventListener('submit', this.submitForm);
+    }
+    submitForm(e) {
+        e.preventDefault();
+        const title = this.titleInput.value;
+        console.log(this.titleInput.value);
+    }
 }
+__decorate([
+    autobindDecorator
+], Input.prototype, "submitForm", null);
 const test = new Input;
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,52 @@
+// Decorator for autobind:
+function autobindDecorator(_: any, _2: any, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn
+        }
+    }
+    return adjustedDescriptor;
+}
 
-// function decoratorFactory () {
-//     return function decorator(target: any, descriptor: any) {
-//         // const firstTemplate = new Input('project-input');
-//         console.log('target: ', target);
-//         console.log('descriptor: ', descriptor)
-
-//     }
-// }
-
-// @decoratorFactory()
+// Input Class:
 class Input {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLElement;
     element: HTMLFormElement;
+    titleInput: HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    peopleInput: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLElement;
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input'
         this.render();
+        this.titleInput = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInput = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInput = this.element.querySelector('#people')! as HTMLInputElement;
+        this.listen();
+
     }
 
-    render() {
+    private render() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+
+    private listen() {
+        this.element.addEventListener('submit', this.submitForm)
+    }
+
+    @autobindDecorator
+    submitForm(e: Event) {
+        e.preventDefault();
+        const title: any = this.titleInput.value;
+        console.log(this.titleInput.value);
     }
 }
 
